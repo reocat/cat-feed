6267a295b648a7ae979fb77d34be6b9cc9074856
reocat <111279389+reocat@users.noreply.github.com>
2023-10-06 20:17:02 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,13 +1,12 @@
-/** @type {import('next').NextConfig} */
-
+/**
+ * @type {import('next').NextConfig}
+ */
 const nextConfig = {
-    output: export,
-    reactStrictMode: true,
-    // Note: This feature is required to use NextJS Image in SSG mode.
-    // See https://nextjs.org/docs/messages/export-image-api for different workarounds.
-    images: {
-      unoptimized: true,
-    },
+    output: 'export',
+    // Optional: Add a trailing slash to all paths `/about` -> `/about/`
+    // trailingSlash: true,
+    // Optional: Change the output directory `out` -> `dist`
+    // distDir: 'dist',
   }
-  
+   
   module.exports = nextConfig
