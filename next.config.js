d2cee9a5c5b21347ade78dff72c0596a1a367166
reocat <ng.ct_ml@tuta.io>
2023-10-15 14:03:18 +0300

    add android

--- next.config.js
+++ next.config.js
@@ -1,12 +1,11 @@
-/**
- * @type {import('next').NextConfig}
- */
+/** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'export',
-    // Optional: Add a trailing slash to all paths `/about` -> `/about/`
-    // trailingSlash: true,
-    // Optional: Change the output directory `out` -> `dist`
-    // distDir: 'dist',
-  }
-   
-  module.exports = nextConfig
+  output: "export",
+	reactStrictMode: true,
+	swcMinify: true,
+	images: {
+		unoptimized: true
+	}
+};
+
+module.exports = nextConfig;
